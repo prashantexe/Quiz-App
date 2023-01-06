90e15a36e02e035b64ed2915b2922a8e6fc19f94
Prashant <prashantd049@gmail.com>
2023-01-06 23:08:25 +0530

    Commit

--- script.js
+++ script.js
@@ -44,3 +44,30 @@ const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0
 let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
