f1700d25eddc164def3d896af0a57dfadacc3042
Prashant <prashantd049@gmail.com>
2023-01-06 23:07:55 +0530

    Commit

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ const quizData = [
         d: "Helicopters Terminals Motorboats Lamborginis",
         correct: "a",
     },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
